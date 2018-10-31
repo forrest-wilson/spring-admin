ca59dcf5119159bba43cd99f76c1cc48f7a455e3
Forrest Wilson <forrest.wilson@me.com>
2018-11-01 08:59:42 +1300

    Changed default URL back to the live domain

--- src/store.js
+++ src/store.js
@@ -3,8 +3,8 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-// let url = 'http://springapi.wlg.cloudapp.goapollo.nz'
-let url = 'http://localhost:5656'
+let url = 'http://springapi.wlg.cloudapp.goapollo.nz'
+// let url = 'http://localhost:5656'
 
 export default new Vuex.Store({
   state: {
